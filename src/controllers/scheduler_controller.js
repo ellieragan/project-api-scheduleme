0617a392eb933571124366dbb73929ea02a8fd6d
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-06-02 17:31:23 -0400

    create scheduler refactor done

--- src/controllers/scheduler_controller.js
+++ src/controllers/scheduler_controller.js
@@ -1,15 +1,35 @@
 import Scheduler from '../models/scheduler_model';
+import { createEvent } from './event_controller';
 
-/* Main Page is where user creator selects days of interest */
-/* Once they do, and click a bytton, maybe Create Event, scheduler is added to DB */
-/* Scheduler is created with the creator's email, days of interest, and calendar ID */
-/* That way, we can get the id of the scheduler on Click event */
+// create an empty calendar to use to initialize scheduler
+const start = 9; // start and end times hard coded for now
+const end = 18;
+
+const createBlankEvents = async () => {
+  const timeList = [];
+  for (let t = start; t < end + 1; t += 1) { // hours specified
+    for (let b = 0; b < 4; b += 1) { // 15 minute increments (0 is on the dot, 1 is 15m, 2 is 30m, 3 is 45m)
+      for (let d = 0; d < 7; d += 1) { // 7 days in week
+        const timeString = `${String(d)}.${String(t)}.${String(b)}`;
+        const timeItem = ({
+          key: timeString, day: d, time: t, block: b, count: 0, available: [],
+        });
+        const temp = createEvent(timeItem);
+        timeList.push(temp);
+      }
+    }
+  }
+  return Promise.all(timeList); // need this so return actual events and not promises
+};
+
+/* Scheduler is created with the creator's email and blank events from code above */
 /* Update the main view's url to contain the calendar ID, so every calendar is different */
-export async function createScheduler(schedulerFields) {
+export async function createScheduler(schedulerInput) {
+  const times = await createBlankEvents();
   const scheduler = new Scheduler();
-  scheduler.creator = schedulerFields.creator;
-  scheduler.users = schedulerFields.users;
-  scheduler.events = schedulerFields.events;
+  scheduler.creator = schedulerInput.creator;
+  scheduler.title = schedulerInput.title;
+  scheduler.events = times;
   try {
     const savedScheduler = await scheduler.save();
     return savedScheduler; // return scheduler
@@ -19,7 +39,7 @@ export async function createScheduler(schedulerFields) {
 }
 
 // updated after a user goes through authentication
-// may bnot neeed this
+// may not need this
 // export async function joinScheduler(schedulerId, userInfo) {
 //   // find the calendar user is supposed to fill out. Will be helpful in refactoring share... maybe??
 //   const scheduler = await Scheduler.findById(schedulerId);
@@ -47,7 +67,6 @@ export async function updateScheduler(id, schedulerFields) {
   try {
     const scheduler = await Scheduler.findById(id);
     scheduler.creator = schedulerFields.creator;
-    scheduler.users = schedulerFields.users;
     scheduler.events = schedulerFields.events;
     const savedScheduler = await scheduler.save();
     return savedScheduler; // return scheduler
